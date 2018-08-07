4cc8d865eef48be22f6ce71c66f5480be0b1b0aa
ecavina <eugenio.cavina@studio.unibo.it>
2018-08-07 15:34:06 +0200

    added login

--- routes/dbRoutes.js
+++ routes/dbRoutes.js
@@ -3,11 +3,10 @@ const url = process.env.MONGODB_URI || "mongodb://heroku_sh9pbrkt:8u0np9l4apmmp6
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-
 let dbPoolConnection;
-MongoClient.connect(url, {poolSize:10} ,function(err, db) {
+MongoClient.connect(url, {poolSize:10, useNewUrlParser: true} , function(err, db) {
   if (err) throw err;
-  dbPoolConnection = db.db("heroku_sh9pbrkt");
+  dbPoolConnection = db.db(db.s.options.dbName);
 });
 
 module.exports = (function() {
@@ -29,8 +28,32 @@ module.exports = (function() {
                     if (err) {res.send(err);throw err;}
                     res.send(dbRes);
                 });
-                console.log("Registered new user: "+userData);
+                console.log("Registered new user: ");
+                for (var property in userData){
+                    console.log(property + " > " + userData[property]);
+                }
+                req.session.userId=userData["_id"];
             });
+        } else {
+            res.status(400).send("Empty Fields");
+        }
+    });
+
+    dbRoutes.get("/login", function(req, res) {
+        if(req.headers.email && req.headers.password) {
+            dbPoolConnection.collection("Users").findOne({email : req.headers.email}, function(err, dbResult) {
+                if (err) {res.send(err);throw err;}
+                bcrypt.compare(req.headers.password, dbResult["password"], function(err, cryptResult) {
+                    if (cryptResult === true) {
+                        req.session.userId=dbResult["_id"];
+                        res.send("Ok");
+                      } else {
+                        res.send("Wrong password");
+                      }
+                });
+            });
+        } else {
+            res.status(400).send("Empty Fields");
         }
     });
 
--- server.js
+++ server.js
@@ -2,9 +2,16 @@ const express = require('express');
 const path = require('path');
 const serveStatic = require('serve-static');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 
 let app = express();
 app.use(serveStatic(__dirname + "/dist"));
+
+app.use(session({
+  secret: 'work hard',
+  resave: true,
+  saveUninitialized: false
+}));
 
 app.use(bodyParser.urlencoded({extended: true}));//TODO cercare cosa significa extended
 app.use(bodyParser.json());
