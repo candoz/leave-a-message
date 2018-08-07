fc4d24bbe3fc15c298e4e165b0f879d531c784e4
ecavina <eugenio.cavina@studio.unibo.it>
2018-08-07 12:20:16 +0200

    implemented users registration

--- routes/dbRoutes.js
+++ routes/dbRoutes.js
@@ -1,17 +1,13 @@
 const MongoClient = require('mongodb').MongoClient;
 const url = process.env.MONGODB_URI || "mongodb://heroku_sh9pbrkt:8u0np9l4apmmp6ur55t1o588p0@ds113452.mlab.com:13452/heroku_sh9pbrkt";
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 
 let dbPoolConnection;
 MongoClient.connect(url, {poolSize:10} ,function(err, db) {
   if (err) throw err;
   dbPoolConnection = db.db("heroku_sh9pbrkt");
-
-  //test
-    dbPoolConnection.collection("Comments").find({}).toArray(function(err, result) {
-        if (err) throw err;
-        console.log(result);
-        db.close();
-    });
 });
 
 module.exports = (function() {
@@ -19,8 +15,23 @@ module.exports = (function() {
 
     let dbRoutes = require('express').Router();
 
-    dbRoutes.get('/', function (req, res) {
-        res.send('Hello db routes!');
+    dbRoutes.post('/users', function (req, res) { 
+        if(req.body.email && req.body.password && req.body.nickname) {
+            bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+                if(err) console.log(err);
+                let userData = {
+                    email: req.body.email,
+                    nickname: req.body.nickname,
+                    password: hash
+                }
+                
+                dbPoolConnection.collection("Users").insertOne(userData, function(err, dbRes) {
+                    if (err) {res.send(err);throw err;}
+                    res.send(dbRes);
+                });
+                console.log("Registered new user: "+userData);
+            });
+        }
     });
 
     return dbRoutes;
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const path = require('path');
 const serveStatic = require('serve-static');
+const bodyParser = require('body-parser');
 
 let app = express();
 app.use(serveStatic(__dirname + "/dist"));
+
+app.use(bodyParser.urlencoded({extended: true}));//TODO cercare cosa significa extended
+app.use(bodyParser.json());
 
 let dbRoutes = require('./routes/dbRoutes');
 app.use('/db', dbRoutes);
